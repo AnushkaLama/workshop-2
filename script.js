bd2549c50c0874b0629b3f97289693c4fcebc5f4
AnushkaLama <np03cy4s250003@heraldcollege.edu.np>
2025-11-28 21:02:00 +0545

    Minor changes on css and js

--- script.js
+++ script.js
@@ -1,42 +1,47 @@
 let header = document.querySelector('header');
 
 setInterval(() => {
-    header.style.color = header.style.color === 'red' ? 'pink' : 'red';
+    header.style.color = header.style.color === 'red' ? 'darkred' : 'red';
 }, 1000);
+
 let form = document.querySelector('form');
-let name = document.getElementById('name');
-let email = document.getElementById('email');
-let message = document.getElementById('message');
-let label = document.querySelectorAll('label');
+let nameInput = document.getElementById('name');
+let emailInput = document.getElementById('email');
+let messageInput = document.getElementById('message');
+let labels = document.querySelectorAll('label');
 let msg = document.querySelector('.msg');
+
 form.addEventListener('submit', function(event) {
-    if (name.value === '' || email.value === '') {
+    if (nameInput.value.trim() === '' || emailInput.value.trim() === '') {
         event.preventDefault();
-        label.forEach(function(lbl) {
+        
+        labels.forEach(function(lbl) {
             lbl.style.color = 'red';
         });
+        
         msg.style.display = 'block';
-        msg.textContent = 'please fill in all the info before submitting.';
+        msg.textContent = 'Please fill in all the info before submitting.';
     }
     else {
-        event.preventDefault();
-        label.forEach(function(lbl) {
-            lbl.style.color = 'white';
+        event.preventDefault(); o
+        
+        labels.forEach(function(lbl) {
+            lbl.style.color = 'white'; 
         });
+        
         msg.style.display = 'block';
+        msg.style.color = '#7fff00';
         msg.textContent = 'Form submitted successfully!';
+        
         form.reset();
     }
 });
 
 let ham = document.querySelector('.ham');
 let navUl = document.querySelector('nav ul');
+
 ham.addEventListener('click', function() {
     ham.textContent = ham.textContent === '☰' ? '✖' : '☰';
-    if (navUl.style.display === 'flex') {
-        navUl.style.display = 'none';
-
-    } else {
-        navUl.style.display = 'flex';
-    }
+    
+    navUl.classList.toggle('active');
 });
